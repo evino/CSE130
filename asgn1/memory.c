918388c3e4062ed886644860e33caa78c12276b0
EvinPC <evinodisho@gmail.com>
2023-01-17 23:15:33 -0800

    get should be good now

--- asgn1/memory.c
+++ asgn1/memory.c
@@ -5,58 +5,39 @@
 #include <limits.h>
 
 #define MAX_LEN 8192
-//#define BUFF_LEN 1024
 
 int main() {
-    // char commandBuffer[MAX_LEN];
-    // char command[MAX_LEN];
-    // char file[MAX_LEN];
     char buffer[MAX_LEN] = "";
     char command[MAX_LEN] = "";
     char file[MAX_LEN] = "";
-    
+
     ssize_t bytes = read(0, buffer, MAX_LEN);
-    ssize_t byteCount = bytes;
     sscanf(buffer, "%s %s", command, file);
-    if (strlen(file) > PATH_MAX) {  // File name len exceed
+
+    if (strlen(file) > PATH_MAX) { // File name len exceed
         write(2, "Invalid Command\n", strlen("Invalid Command\n"));
         return 1;
     }
-    if (byteCount > MAX_LEN) {
-        write(2, "Operation Failed\n", strlen("Operation Failed\n"));
-        return 1;
-    }
-
-    //printf("%zu\n", bytes);
 
     if (strcmp(command, "get") == 0) {
         int fd = open(file, O_RDWR);
-        if (fd == -1) {  // Invalid File
+        if (fd == -1) { // Invalid File
             write(2, "Invalid Command\n", strlen("Invalid Command\n"));
             return 1;
         }
 
-
         // SWITCH TO USE ONE BUFFER
         while (bytes > 0) {
             bytes = read(fd, buffer, 4096);
-            byteCount += bytes;
-            //printf("%zu\n", bytes);
-            if (byteCount > MAX_LEN) {
-                write(2, "Operation Failed\n", strlen("Operation Failed\n"));
-                return 1;
-            }
             write(1, buffer, bytes);
-            //printf("\n\nBuff: %s\n", buffer);
-
-        //     //write(1, "\n", strlen(buffer));
         }
 
-        //printf("%zu\n", byteCount);
-        //printf("%s\n", file);
+        if (bytes == -1) { // If write fails
+            write(2, "Operation Failed\n", strlen("Operation Failed\n"));
+            return 1;
+        }
 
         close(fd);
-
     } else if (strcmp(command, "set") == 0) {
         printf("set\n");
     } else {
