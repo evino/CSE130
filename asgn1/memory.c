fef5c2d41ae6cbe0afea641b539bcf1b74a84854
EvinPC <evinodisho@gmail.com>
2023-01-17 22:54:17 -0800

    made buffer bigger

--- asgn1/memory.c
+++ asgn1/memory.c
@@ -4,7 +4,7 @@
 #include <fcntl.h>
 #include <limits.h>
 
-#define MAX_LEN 1024
+#define MAX_LEN 8192
 //#define BUFF_LEN 1024
 
 int main() {
@@ -15,25 +15,44 @@ int main() {
     char command[MAX_LEN] = "";
     char file[MAX_LEN] = "";
     
-    ssize_t bytes = read(0, buffer, sizeof(buffer));
+    ssize_t bytes = read(0, buffer, MAX_LEN);
+    ssize_t byteCount = bytes;
     sscanf(buffer, "%s %s", command, file);
-
-    int fd = open(file, O_RDWR);
-    if ((fd == -1) || (strlen(file) >= PATH_MAX)) {
+    if (strlen(file) > PATH_MAX) {  // File name len exceed
         write(2, "Invalid Command\n", strlen("Invalid Command\n"));
-        //close(fd);
+        return 1;
+    }
+    if (byteCount > MAX_LEN) {
+        write(2, "Operation Failed\n", strlen("Operation Failed\n"));
         return 1;
     }
 
+    //printf("%zu\n", bytes);
+
     if (strcmp(command, "get") == 0) {
+        int fd = open(file, O_RDWR);
+        if (fd == -1) {  // Invalid File
+            write(2, "Invalid Command\n", strlen("Invalid Command\n"));
+            return 1;
+        }
+
 
         // SWITCH TO USE ONE BUFFER
         while (bytes > 0) {
-            bytes = read(fd, buffer, sizeof(buffer));
+            bytes = read(fd, buffer, 4096);
+            byteCount += bytes;
+            //printf("%zu\n", bytes);
+            if (byteCount > MAX_LEN) {
+                write(2, "Operation Failed\n", strlen("Operation Failed\n"));
+                return 1;
+            }
             write(1, buffer, bytes);
+            //printf("\n\nBuff: %s\n", buffer);
+
         //     //write(1, "\n", strlen(buffer));
         }
 
+        //printf("%zu\n", byteCount);
         //printf("%s\n", file);
 
         close(fd);
