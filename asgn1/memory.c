514e2df959f141c070971e8af9d4786ad7efe0e2
EvinPC <evinodisho@gmail.com>
2023-01-17 20:43:29 -0800

    Need to play around with buffer

--- asgn1/memory.c
+++ asgn1/memory.c
@@ -2,34 +2,46 @@
 #include <string.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <limits.h>
 
-#define MAX_LEN 64
-#define BUFF_LEN 1024
+#define MAX_LEN 1024
+//#define BUFF_LEN 1024
 
 int main() {
-    char commandBuffer[MAX_LEN] = "";
+    // char commandBuffer[MAX_LEN];
+    // char command[MAX_LEN];
+    // char file[MAX_LEN];
+    char buffer[MAX_LEN] = "";
     char command[MAX_LEN] = "";
     char file[MAX_LEN] = "";
-    char buffer[BUFF_LEN];
-    read(0, commandBuffer, 32);
-    sscanf(commandBuffer, "%s %s", command, file);
+    
+    ssize_t bytes = read(0, buffer, sizeof(buffer));
+    sscanf(buffer, "%s %s", command, file);
+
     int fd = open(file, O_RDWR);
+    if ((fd == -1) || (strlen(file) >= PATH_MAX)) {
+        write(2, "Invalid Command\n", strlen("Invalid Command\n"));
+        //close(fd);
+        return 1;
+    }
 
     if (strcmp(command, "get") == 0) {
-        if (fd == -1) {
-            write(2, "Invalid Command\n", strlen("Invalid Command\n"));
-            return 1;
+
+        // SWITCH TO USE ONE BUFFER
+        while (bytes > 0) {
+            bytes = read(fd, buffer, sizeof(buffer));
+            write(1, buffer, bytes);
+        //     //write(1, "\n", strlen(buffer));
         }
 
-        //read(fd, buffer, strlen(buffer));
-        write(1, buffer, read(fd, buffer, strlen(buffer)));
-
         //printf("%s\n", file);
+
+        close(fd);
 
     } else if (strcmp(command, "set") == 0) {
         printf("set\n");
     } else {
-        printf("ERR\n");
+        write(2, "Invalid Command\n", strlen("Invalid Command\n"));
         return 1;
     }
     return 0;
