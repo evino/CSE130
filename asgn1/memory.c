7ff8f664b1f5aedea1a0f673de49500925f3f6ec
EvinPC <evinodisho@gmail.com>
2023-01-20 23:55:34 -0800

    formatted. still not working for a few cases

--- asgn1/memory.c
+++ asgn1/memory.c
@@ -16,8 +16,6 @@ int invalid() {
 int main() {
     char buffer[BUFFER_SIZE] = ""; // Might need to initialize to silence valgrind error
 
-    // char *invalid = "Invalid Command\n";
-
     // read command and file from STDIN
     // Can read one byte at a time, and loop till there is a newline (in buffer)
     // int bytes_read = read(0, buffer, BUFFER_SIZE);
@@ -56,12 +54,6 @@ int main() {
     if (S_ISDIR(fileCheck.st_mode) != 0) { // File is a directory, therefore not valid
         return invalid();
     }
-    // printf("DB2\n");
-
-    // If file is blank
-    // if (strcmp(file, "") == 0) {
-    //     return invalid();
-    // }
 
     int write_fd = 999;
     int read_fd = 999;
@@ -71,8 +63,6 @@ int main() {
         read_fd = 0;
     } else {
         return invalid();
-        // write(2, invalid, strlen(invalid));
-        // return 1;
     }
 
     if (strlen(file) > PATH_MAX) {
@@ -86,22 +76,13 @@ int main() {
 
     // Make sure file is closed
     int fd = open(file, O_RDWR);
-    // printf("DB\n");
     if (fd == -1 && write_fd == 1) { // File does not exist
         return invalid();
-        //write(2, invalid, strlen(invalid));
-        //return 1;
     } else if (write_fd == 1 && fd > -1) {
         read_fd = fd;
     } else if (read_fd == 0) { // If command is "set"
-        // if (unlink())
-        // open(file, O_CREAT O_RDWR | O_TRUNC, 0777);
-        //close(fd);
-        //write_fd = creat(file, 0777);
         write_fd = open(file, O_CREAT | O_RDWR | O_TRUNC, 0777);
         fd = write_fd;
-        // printf("right before check\n");
-        // printf("write_fd: %d\n", write_fd);
         if (write_fd == -1) { // Make sure creat worked
             write(2, "Operation Failed\n", strlen("Operation Failed\n"));
             return 1;
@@ -131,8 +112,6 @@ int main() {
         }
     } while (bytes_read > 0);
 
-    //printf("%d", read_fd);
-
     // MAKE SURE TO CLOSE PROPER FILES, AND CHECK IT!!!
 
     if (close(fd) == -1) { // THIS IF IS CAUSING THE FAIL WHEN CALLING SET ON NEW FILE
@@ -146,71 +125,3 @@ int main() {
 
     return 0;
 }
-
-/*
-    bytes_read = 0;
-
-
-    do {
-        bytes_read = read(fd, buffer, BUFFER_SIZE);
-
-        if (bytes_read < 0) {
-            write(2, "Operation Failed\n", strlen("Operation Failed\n"));
-            return 1;
-        } else if (bytes_read > 0) {
-            ssize_t bytes_written = 0;
-
-           // do {
-           //     ssize_t bytes = write()
-           // }
-        }
-    }
-
-}
-*/
-
-/*
-
-    if (strlen(file) > PATH_MAX) { // File name len exceed
-        write(2, "Invalid Command\n", strlen("Invalid Command\n"));
-        return 1;
-    }
-
-    if (strcmp(command, "get") == 0) {
-        if (fd == -1) { // Invalid File
-            write(2, "Invalid Command\n", strlen("Invalid Command\n"));
-            return 1;
-        }
-
-        // SWITCH TO USE ONE BUFFER
-        while (bytes_read > 0 && writeVal > -1) {
-            bytes_read = read(fd, buffer, 4096);
-            writeVal = write(1, buffer, bytes_read);
-        }
-
-        close(fd); // This was moved up
-
-        if (bytes_read == -1 || writeVal == -1) { // If write fails
-            write(2, "Operation Failed\n", strlen("Operation Failed\n"));
-            return 1;
-        }
-
-    } else if (strcmp(command, "set") == 0) {
-        //if (fd == -1) {
-        //fd = open(file, S_IRWXU | O_RDWR | O_TRUNC | O_CREAT);
-        fd = open(file, O_CREAT | O_TRUNC | O_WRONLY, 0777);
-        // while (bytes_read > 0 && writeVal > -1) {
-        while (bytes_read >= 0) {
-            bytes_read = read(0, buffer, 4096);
-            writeVal = write(fd, buffer, bytes_read);
-        }
-
-        return 0;
-        //}
-    } else {
-        write(2, "Invalid Command\n", strlen("Invalid Command\n"));
-        return 1;
-    }
-    return 0;
-}
-*/
