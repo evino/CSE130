75e869a9e749dcb2c44f622c933c26f0ffe58bd9
EvinPC <evinodisho@gmail.com>
2023-01-28 20:36:35 -0800

    invalid port check

--- asgn2/httpserver.c
+++ asgn2/httpserver.c
@@ -1,3 +1,15 @@
+#include "helpers.h"
+#include <stdlib.h>
+
+
 int main(int argc, char **argv) {
+    if (argc != 2) {
+        return (invalid_port());
+    }
+
+    int port = atoi(argv[1]);
+    if (port < 1 || port > 65535) {
+        return (invalid_port());
+    }
     return 0;
 }
