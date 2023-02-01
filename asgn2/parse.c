8498117b197cb6c11843f8b4fd9cd4c30270d9c3
Evin <edodisho@ucsc.edu>
2023-01-31 23:31:46 -0800

    regex

--- asgn2/parse.c
+++ asgn2/parse.c
@@ -1,1 +1,30 @@
-#define REGEX "^([a-zA-Z]{1,8}) (/[a-zA-Z0-9._]{2,63}) (HTTP/[0-9].[0-9])\r\n"
+
+#include <regex.h>
+#include <assert.h>
+
+#include "parse.h"
+#include "helpers.h"
+
+
+// FOR DB
+#include <stdio.h>
+
+
+#define REQUEST_REGEX "^([a-zA-Z]{1,8}) (/[a-zA-Z0-9._]{2,63}) (HTTP/[0-9].[0-9])\r\n"
+
+void request_parse(Command *com) {
+    regex_t re;
+    regmatch_t matches[5];
+    int rc;
+
+
+    rc = regcomp(&re, REQUEST_REGEX, REG_EXTENDED);
+    assert(!rc);  // MAY NEED INTERNAL SERVER ERROR HERE
+    
+    rc = regexec(&re, com->buf, 5, matches, 0);
+    if (rc == 0) {
+        com->method = com->buf + matches[0].rm_so;
+    }
+
+    printf("%s\n", com->method);
+}
