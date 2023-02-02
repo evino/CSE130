d6062eae8a7e752037f7483659e5f2dd2c450689
Evin <edodisho@ucsc.edu>
2023-02-02 00:58:55 -0800

    Fixed regex match

--- asgn2/parse.c
+++ asgn2/parse.c
@@ -8,21 +8,37 @@
 // FOR DB
 #include <stdio.h>
 
-#define REQUEST_REGEX "^([a-zA-Z]{1,8}) (/[a-zA-Z0-9._]{2,63}) (HTTP/[0-9].[0-9])\r\n"
+#define REQUEST_REGEX "^([a-zA-Z]{1,8}) (/[a-zA-Z0-9._]{1,63}) (HTTP/[0-9].[0-9])\r\n"
 
 void request_parse(Command *com) {
     regex_t re;
-    regmatch_t matches[5];
+    regmatch_t matches[4];
     int rc;
 
     rc = regcomp(&re, REQUEST_REGEX, REG_EXTENDED);
     assert(!rc); // MAY NEED INTERNAL SERVER ERROR HERE
 
-    rc = regexec(&re, (char *) com->buf, 5, matches, 0);
+    rc = regexec(&re, (char *) com->buf, 4, matches, 0);
     if (rc == 0) {
-        com->method = com->buf;
-        com->method[matches[1].rm_eo] = '\0';  // NEED TO FIGURE OUT WHY method is NULL
+        com->request_line = com->buf;
+
+        com->method = com->buf + matches[1].rm_so;
+        com->method[matches[1].rm_eo - matches[1].rm_so] = '\0';
+
+        com->URI = com->buf + matches[2].rm_so;
+        com->URI[matches[2].rm_eo - matches[2].rm_so] = '\0';
+
+        com->version = com->buf + matches[3].rm_so;
+        com->version[matches[3].rm_eo - matches[3].rm_so] = '\0';
+        //   com->method = &REQUEST_REGEX[matches[1].rm_so];// - matches[1].rm_so;
+        //com->method[matches[1].rm_eo] = '\0';  // NEED TO FIGURE OUT WHY method is NULL
+    } else {
+        printf("NOOOOOOO MATCHOOOOO\n");
     }
-    printf("db: %s\n", com->buf);
+    //printf("db: %s\n", matches[0]);
+    // DEBUG PRINTS
     printf("method: %s\n", com->method);
+    printf("URI: %s\n", com->URI);
+    printf("version: %s\n", com->version);
 }
+// CALL REGFREE
