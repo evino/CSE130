d700668455be9393f83e361035f670b4b4dd96a5
Evin <edodisho@ucsc.edu>
2023-02-15 13:12:06 -0800

    basic for now

--- asgn3/test.c
+++ asgn3/test.c
@@ -1,13 +1,31 @@
 #include <stdio.h>
+#include <pthread.h>
 
 #include "queue.h"
 
 int main() {
+    // pthread_t t1;
+
+
     queue_t *myQueue = queue_new(15);
 
-    int x = 4;
-    queue_push(myQueue, &x);
+    uintptr_t x = 4;
+    // typedef struct {
+    //     queue_t *q;
+    //     void *elem;
+    // } args;
+    // args queueArgs;
+    // queueArgs.q = myQueue;
+
+    // pthread_create(&t1, NULL, queue_push, [myQueue, (void*)x]);
     printf("%d\n", getCount(myQueue));
+    queue_push(myQueue, (void *)&x);
+    printf("%d\n", getCount(myQueue));
+    uintptr_t r;
+    queue_pop(myQueue, (void **)&r);
+    printf("%d\n", getCount(myQueue));
+
+
 
     return 0;
 
