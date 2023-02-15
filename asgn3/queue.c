612b5a906168b7dded81419c805bd8234d7d418f
Evin <edodisho@ucsc.edu>
2023-02-15 01:09:15 -0800

    Need to test push()

--- asgn3/queue.c
+++ asgn3/queue.c
@@ -2,8 +2,9 @@
 #include <pthread.h>
 #include "queue.h"
 
-typedef struct queue{
-    int length;
+struct queue{
+    int count;
+    int size;
 
     int front;
     int rear;
@@ -14,21 +15,54 @@ typedef struct queue{
     pthread_mutex_t mutex_push;
     pthread_mutex_t mutex_pop;
 
-    void **arr;
-} queue_t;// Need queue here?
+    int cv_rc;
+    pthread_cond_t cv; // Might need seperate ones for push() & pop()
 
-// typedef queue queue_t;
+    void **arr;
+};// Need queue here?
+
+
+
 
 queue_t *queue_new(int size) {
     queue_t *q = malloc(sizeof(queue_t));
-    q->length = 0;
+    q->count = 0;
+    q->size = size;
+
     q->front = 0;
     q->rear = 0;
 
     q->arr = malloc(size * sizeof(void *));
 
     q->push_rc = pthread_mutex_init(&(q->mutex_push), NULL);
+    assert(!(q->push_rc));
+
     q->pop_rc = pthread_mutex_init(&(q->mutex_pop), NULL);
+    assert(!(q->pop_rc));
+
+    q->cv_rc = pthread_cond_init(&(q->cv), NULL);
+    assert(!(q->cv_rc));
 
     return q;
+}
+
+// delete() {
+// POP() till empty
+// Then call free() on queue itself
+
+
+
+bool queue_push(queue_t *q, void *elem) {
+    if (q == NULL) {
+        return false;
+    }
+
+    while (q->count == q->size) {
+        pthread_cond_wait(&(q->cv), &(q->mutex_push));
+    }
+    q->arr[q->front] = elem;
+    q->front = (q->front + 1) % q->size;
+    q->count += 1;
+
+    return true;
 }
