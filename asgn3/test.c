eb851dea1ba02cd7223f1a85dfceaf4826ad8630
Evin <edodisho@ucsc.edu>
2023-02-23 15:16:06 -0800

    Still working on it

--- asgn3/test.c
+++ asgn3/test.c
@@ -11,23 +11,27 @@ typedef struct {
 } Push_Args;
 
 void *push(void *args) {
-    Push_Args *pushInfo = args;
-    queue_push(pushInfo->q, pushInfo->elem);
+    //Push_Args *pushInfo = args;
+    //queue_push(pushInfo->q, pushInfo->elem);
+    // (Push_Args *)args;
+    queue_push(((Push_Args *)args)->q, (void *)((Push_Args *)args)->elem);
     return NULL;
 }
 
 int main() {
-    queue_t *queue = queue_new(queue_size);
-    if (queue == NULL) {
+    queue_t *myQueue = queue_new(queue_size);
+    if (myQueue == NULL) {
         return 1;
     }
 
     pthread_t t1;
 
-    Push_Args *t1_arg;
-    t1_arg->q = queue;
-    uintptr_t x = 15;
-    t1_arg->elem = &x;
+    uintptr_t x = 1;
+    Push_Args *t1_arg = {myQueue, &x};
+    t1_arg->q = myQueue;
+    // t1_arg->q = queue;
+    // uintptr_t x = 15;
+    // t1_arg->elem = &x;
 
     pthread_create(&t1, NULL, push, (void *)&t1_arg);
 
