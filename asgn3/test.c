bd6389efee6346d07a3cccf557aba45ef5cf059b
Evin <edodisho@ucsc.edu>
2023-02-23 16:26:50 -0800

    fied one part

--- asgn3/test.c
+++ asgn3/test.c
@@ -1,22 +1,23 @@
 #include <stdio.h>
 #include <pthread.h>
+#include <stdlib.h>
 
 #include "queue.h"
 
 #define queue_size 5
 
-typedef struct {
+struct Push_Args {
     queue_t *q;
     void *elem;
-} Push_Args;
+};
 
-void *push(void *args) {
-    //Push_Args *pushInfo = args;
-    //queue_push(pushInfo->q, pushInfo->elem);
-    // (Push_Args *)args;
-    queue_push(((Push_Args *)args)->q, (void *)((Push_Args *)args)->elem);
-    return NULL;
-}
+// void *push(void *args) {
+//     // struct Push_Args *pushInfo = args;
+//     //queue_push(pushInfo->q, pushInfo->elem);
+//     // (Push_Args *)args;
+//     // queue_push(((Push_Args *)args)->q, (void *)((Push_Args *)args)->elem);
+//     return NULL;
+// }
 
 int main() {
     queue_t *myQueue = queue_new(queue_size);
@@ -27,13 +28,16 @@ int main() {
     pthread_t t1;
 
     uintptr_t x = 1;
-    Push_Args *t1_arg = {myQueue, &x};
+    // struct Push_Args *t1_arg = {myQueue, &x};
+    struct Push_Args *t1_arg;
+    t1_arg = malloc(sizeof(t1_arg));
     t1_arg->q = myQueue;
+
     // t1_arg->q = queue;
     // uintptr_t x = 15;
     // t1_arg->elem = &x;
 
-    pthread_create(&t1, NULL, push, (void *)&t1_arg);
+    // pthread_create(&t1, NULL, push, (void *)&t1_arg);
 
 
 }
