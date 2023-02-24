ce30f7b2678c212c267b15cdc08f5ea0d62611d1
Evin <edodisho@ucsc.edu>
2023-02-24 11:31:29 -0800

    Fixed segfaults. Able to push and pop across mult threads

--- asgn3/test.c
+++ asgn3/test.c
@@ -4,8 +4,6 @@
 
 #include "queue.h"
 
-#define queue_size 5
-
 typedef struct Push_Args {
     queue_t *q;
     void *elem;
@@ -19,18 +17,16 @@ struct Pop_Args {
 void *push(void *args) {
     Push_Args *pushArgs = (Push_Args *) args;
 
-    // queue_t *q = pushArgs->q;
     if (pushArgs->q == NULL) {
-        fprintf(stderr, "IT'S NULL BRUH\n");
+        fprintf(stderr, "NULL\n");
     }
 
-    fprintf(stderr, "%lu\n", (uintptr_t) pushArgs->elem);
-    fprintf(stderr, "db\n");
+    fprintf(stderr, "Pushed: %lu\n", (uintptr_t)pushArgs->elem);
+    // fprintf(stderr, "db\n");
     // uintptr_t intgr = (uintptr_t)pushArgs->elem;
 
     queue_push(pushArgs->q, (void *) pushArgs->elem);
 
-    fprintf(stderr, "db2\n");
 
     return NULL;
 }
@@ -38,7 +34,10 @@ void *push(void *args) {
 void *pop(void *args) {
     struct Pop_Args *popArgs = (struct Pop_Args *) args;
 
+    fprintf(stderr, "db\n");
     queue_pop(popArgs->q, (void *) (popArgs->elem));
+
+    fprintf(stderr, "Popped: %lu\n", (uintptr_t)(*(popArgs->elem)));
 
     return NULL;
 }
@@ -54,19 +53,33 @@ int main() {
     }
 
     uintptr_t one = 1;
-    // struct Push_Args *t1_arg = {myQueue, &x};
+
+    struct Pop_Args *t2_arg = malloc(sizeof(t2_arg));
+    t2_arg->q = myQueue;
+    void *res = NULL;
+    t2_arg->elem = &res;
+
     struct Push_Args *t1_arg = malloc(sizeof(t1_arg));
     t1_arg->q = myQueue;
-    t1_arg->elem = &one;
-    // push((void *)t1_arg);
+    t1_arg->elem =  (void *)one;
 
+    // push((void *)t1_arg);
     // printf("%lu\n", (uintptr_t)t1_arg->elem);
+
+    pthread_t t2;
+    pthread_create(&t2, NULL, pop, (void *)t2_arg);
+
     pthread_t t1;
-    pthread_create(&t1, NULL, push, (void *) &t1_arg);
+    pthread_create(&t1, NULL, push, (void *)t1_arg);
     fprintf(stderr, "thread made\n");
 
     void *rc;
+    void *rc2;
+    pthread_join(t2, &rc2);
+
+
     pthread_join(t1, &rc);
+
 
     // uintptr_t two = 2;
     // struct Push_Args *t2_arg = malloc(sizeof(t2_arg));
