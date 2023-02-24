915395217a08caa20a6f084548b126e05c45ffd6
Evin <edodisho@ucsc.edu>
2023-02-23 21:37:30 -0800

    getting segfault in test file

--- asgn3/test.c
+++ asgn3/test.c
@@ -11,17 +11,37 @@ struct Push_Args {
     void *elem;
 };
 
+struct Pop_Args {
+    queue_t *q;
+    void **elem;
+};
+
 void *push(void *args) {
     struct Push_Args *pushArgs = (struct Push_Args *)args;
 
-    fprintf(stderr, "%lu\n", (uintptr_t)pushArgs->elem);
+    // queue_t *q = pushArgs->q;
 
+    fprintf(stderr, "%lu\n", (uintptr_t)pushArgs->elem);
+            fprintf(stderr, "db\n");
+    // uintptr_t intgr = (uintptr_t)pushArgs->elem;
+
+    queue_push(pushArgs->q, pushArgs->elem);
 
 
     // struct Push_Args *pushInfo = args;
     //queue_push(pushInfo->q, pushInfo->elem);
     // (Push_Args *)args;
     // queue_push(((Push_Args *)args)->q, (void *)((Push_Args *)args)->elem);
+    fprintf(stderr, "db2\n");
+    return NULL;
+}
+
+
+void *pop(void *args) {
+    struct Pop_Args *popArgs = (struct Pop_Args *)args;
+
+    queue_pop(popArgs->q, (void *)(popArgs->elem));
+
     return NULL;
 }
 
@@ -32,16 +52,25 @@ int main() {
     }
 
 
-    unsigned x = 11;
+    uintptr_t one = 1;
     // struct Push_Args *t1_arg = {myQueue, &x};
     struct Push_Args *t1_arg = malloc(sizeof(t1_arg));
     t1_arg->q = myQueue;
-    t1_arg->elem = (void *)(uintptr_t)x;
-    push((void *)t1_arg);
+    t1_arg->elem = (void *)one;
+    // push((void *)t1_arg);
+
     // printf("%lu\n", (uintptr_t)t1_arg->elem);
+    pthread_t t1;
+    pthread_create(&t1, NULL, push, (void *)&t1_arg);
 
-    // pthread_t t1;
+    void *rc;
+    pthread_join(t1, &rc);
 
+    // uintptr_t two = 2;
+    // struct Push_Args *t2_arg = malloc(sizeof(t2_arg));
+    // t2_arg->q = myQueue;
+    // t2_arg->elem = (void *)two;
+    // push
 
     // t1_arg->q = queue;
     // uintptr_t x = 15;
