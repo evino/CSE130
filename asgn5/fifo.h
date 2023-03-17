5435be36bb9059b666fb2162199146c3ad22feca
Evin <edodisho@ucsc.edu>
2023-03-17 11:42:09 -0700

    Starting with FIFO prototypes

---
+++ asgn5/fifo.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <stdbool.h>
+
+typedef struct fifo fifo_t;
+
+// Builds up a new fifo cache
+fifo_t *fifo_new(int size);
+// Need arr to hold cache elems
+// Need history list
+
+// Clears Cache, and frees all
+// memory associated with it
+void fifo_delete(fifo_t **f);
+
+bool fifo_push(fifo_t *f);
+
+// Print out miss type, and update miss count
+void fifo_miss(fifo_t *f);
