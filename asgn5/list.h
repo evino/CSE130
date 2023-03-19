a30d917261d6b3bb54c392482ce9598ffac5a75c
EvinPC <evinodisho@gmail.com>
2023-03-19 14:03:45 -0700

    added a few more prototypes

--- asgn5/list.h
+++ asgn5/list.h
@@ -1,10 +1,12 @@
-#pragma once
+#ifndef LIST_H
+#define LIST_H
 
+#include <stdbool.h>
 
 // Make private
-typedef struct node node_t;
+// typedef struct node node_t;
 
-node_t *node_new(void *elem);
+//node_t *node_new(void *elem);
 
 
 typedef struct list list_t;
@@ -14,3 +16,9 @@ typedef struct list list_t;
 list_t *list_new();
 
 void list_push(list_t *l, void *elem);
+
+void list_delete(list_t *l);
+
+bool list_search(list_t *l, void *elem);
+
+#endif
