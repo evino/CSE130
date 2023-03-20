51ef64e5ce7a3449e7680400a9c979ac5c7b47f6
EvinPC <evinodisho@gmail.com>
2023-03-20 01:02:39 -0700

    Need to check for misses/hits

--- asgn5/fifo.c
+++ asgn5/fifo.c
@@ -8,6 +8,8 @@ struct fifo {
     int count;
     queue_t *fifo_cache;
     list_t *fifo_hist;
+    int compulsory_miss;
+    int capacity_miss;
 };
 
 fifo_t *fifo_new(int size) {
@@ -15,12 +17,25 @@ fifo_t *fifo_new(int size) {
     cache->count = 0;
     cache->fifo_cache = queue_new(size);
     cache->fifo_hist = list_new();
+    cache->compulsory_miss = 0;
+    cache->capacity_miss = 0;
 
     return cache;
 }
 
 void fifo_push(fifo_t *cache, void *elem) {
+    if (cache->count == cache->fifo_cache->size) {  // Eviction needs to happen
+        fprintf(stdout, "MISS\n";)
+    }
     cache->count++;
     queue_push(cache->fifo_cache, elem);
     list_push(cache->fifo_hist, elem);
+}
+
+
+bool fifo_search(fifo_t *f, void *target) {
+    for (int i = 0; i < f->count; i++) {
+        if // Need to serach queue here instead
+        
+    }
 }
--- asgn5/fifo.h
+++ asgn5/fifo.h
@@ -8,6 +8,8 @@ fifo_t *fifo_new(int size);
 // Need arr to hold cache elems
 // Need history list
 
+bool fifo_search(fifo_t *f);
+
 // Clears Cache, and frees all
 // memory associated with it
 void fifo_delete(fifo_t **f);
@@ -15,4 +17,4 @@ void fifo_delete(fifo_t **f);
 void fifo_push(fifo_t *f, void *elem);
 
 // Print out miss type, and update miss count
-void fifo_miss(fifo_t *f);
+void fifo_miss(fifo_t *f, void *target);
