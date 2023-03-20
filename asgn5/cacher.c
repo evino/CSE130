40bd77e69a4ed1bc76cb73f465af8c7c712fb639
EvinPC <evinodisho@gmail.com>
2023-03-20 01:01:33 -0700

    Fixed to read till stdin closes

--- asgn5/cacher.c
+++ asgn5/cacher.c
@@ -1,12 +1,19 @@
+#include <unistd.h>
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "fifo.h"
 
+#define BUF_SIZE 4096
+
+char buffer[BUF_SIZE];
+
+
 int main(int argc, char **argv) {
-    if (argc < 2) {
-        fprintf(stderr, "Missing arguments\n");
+    if (argc < 2 || argc > 3) {
+        fprintf(stderr, "Invalid arguments\n");
         return 1;
     }
     uintptr_t cache_size = atol(argv[1]);
@@ -19,15 +26,21 @@ int main(int argc, char **argv) {
         fifo_t *cache = fifo_new(cache_size);
 
         void *elem = NULL;
-        char c = '\0';
+        // char c = '\0';
 
         int bytes_read = 0;
         do {
-            bytes_read = fscanf(stdin, "%c", &c);
-            elem = (void *) &c;
+            bytes_read = read(0, buffer, 8);
+            printf("%d\n", bytes_read);
+            elem = (void *) buffer;
             fifo_push(cache, elem);
+        } while(bytes_read > 0);
+        // do {
+        //     bytes_read = fscanf(stdin, "%c", &c);
+        //     elem = (void *) &c;
+        //     fifo_push(cache, elem);
 
-        } while (bytes_read != EOF);
+        // } while (bytes_read != EOF);
         
         // do {
             
