cab526c4eff983c16a8ef10da7b23a5a2a0c56f6
Evin <edodisho@ucsc.edu>
2023-03-19 21:14:28 -0700

    Testing push function across multiple types

--- asgn5/list_test.c
+++ asgn5/list_test.c
@@ -5,11 +5,15 @@ int main() {
     list_t *myList = list_new();
     uintptr_t x = 15;
     uintptr_t y = 11;
-    uintptr_t z = 99;
+    char *c = "c";
+    // char *d = "d";
+    // uintptr_t z = 99;
     list_push(myList, (void *) x);
     list_push(myList, (void *) y);
+    list_push(myList, (void *) c);
+    
 
-    bool contain = list_contains(myList, (void *) z);
+    bool contain = list_contains(myList, (void *) c);
     if (contain == true) {
         return 0;
     }
