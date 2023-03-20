491116b15014df680d11b06c963f2a02c145063a
EvinPC <evinodisho@gmail.com>
2023-03-19 20:51:19 -0700

    Testing  on multiple nodes

--- asgn5/list_test.c
+++ asgn5/list_test.c
@@ -4,10 +4,12 @@
 int main() {
     list_t *myList = list_new();
     uintptr_t x = 15;
-    // uintptr_t y = 11;
+    uintptr_t y = 11;
+    uintptr_t z = 99;
     list_push(myList, (void *) x);
+    list_push(myList, (void *) y);
 
-    bool contain = list_contains(myList, (void *) x);
+    bool contain = list_contains(myList, (void *) z);
     if (contain == true) {
         return 0;
     }
